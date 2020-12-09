ef2e51eb01535b9462fc147b7ce22e545e3ed7a5
Esteban Araujo-Fervenza <x20170416@student.ncirl.ie>
2020-12-09 20:23:07 +0000

    fisrt commit

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       String[] words ={"january","february","march","april","may","june","july","august","september","october","november","december"};
+    }
+}
