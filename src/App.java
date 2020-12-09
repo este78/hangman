303653cb88def78d5629a5e99367cef741d2e51f
Esteban Araujo-Fervenza <x20170416@student.ncirl.ie>
2020-12-09 20:37:16 +0000

    Array created, random logic implemented

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       String[] words ={"january","february","march","april","may","june","july","august","september","october","november","december"};
+        //vars
+        String[] words ={"january","february","march","april","may","june","july","august","september","october","november","december"};
+        int lives = 6;
+        int randomPick=(int)(Math.random()*words.length);
+        String playWord = words[randomPick];
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(words[i]);
+        }
+        System.out.println(playWord);
     }
 }
